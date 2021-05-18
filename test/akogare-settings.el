916f91b0c22af689e55e9e91f4f0023f8bae9568
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-05-18 16:33:26 -0400

    test: settings: increase timer height

--- test/akogare-settings.el
+++ test/akogare-settings.el
@@ -1,1 +1,5 @@
 (setq speedo-buffer "*Akogare Mario World*")
+(add-hook 'speedo-mode-hook
+          (lambda ()
+            (when (string= (buffer-name) "*Akogare Mario World*")
+              (face-remap-add-relative 'speedo-timer :height 3.0))))
