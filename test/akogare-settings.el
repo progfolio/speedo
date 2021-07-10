8666a4b86f0cab600bcd3f9c4a87ee3a158b378a
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-07-09 22:16:11 -0400

    Update tests, README

--- test/akogare-settings.el
+++ test/akogare-settings.el
@@ -1,6 +1,5 @@
 ;; -*- lexical-binding: t; -*-
-(setq speedo-buffer "*Akogare Mario World*")
-(add-hook 'speedo-mode-hook
-          (lambda ()
-            (when (string= (buffer-name) "*Akogare Mario World*")
-              (face-remap-add-relative 'speedo-timer :height 3.0))))
+(face-remap-add-relative 'speedo-timer :height 3.0)
+(with-current-buffer (get-buffer-create speedo-buffer)
+  (unless (string= (buffer-name) "*Akogare Mario World*")
+    (setq speedo-buffer (rename-buffer "*Akogare Mario World*"))))
