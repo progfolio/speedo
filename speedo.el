progfolio/speedo

commit 042f978550dd948ae135b01725c8cde6be8d1fc4

Guard against OOB cl-subseq index

speedo.el
@@ -865,13 +865,15 @@ Reset timers."
                       (or (when (and target-splits
                                      (or speedo--current-attempt speedo--review))
                             (speedo--relative-time
-                             (speedo--splits-duration (cl-subseq target-splits 0 (1+ index)))
                              (speedo--splits-duration
-                              (cl-subseq (plist-get (if speedo--review
-                                                        (speedo-target-last-attempt)
-                                                      speedo--current-attempt)
-                                                    :splits)
-                                         0 (1+ index)))))
+                              (cl-subseq target-splits 0
+                                         (min (1+ index) (1- (length target-splits)))))
+                             (speedo--splits-duration
+                              (when-let ((splits (plist-get (if speedo--review
+                                                                (speedo-target-last-attempt)
+                                                              speedo--current-attempt)
+                                                            :splits)))
+                                (cl-subseq splits 0 (min (1+ index) (1- (length splits))))))))
                           speedo-text-place-holder)))
                 (when current-line (setq s (propertize s 'comparison-timer t)))
                 (if best-split (propertize s 'face 'speedo-pb) s)))

commit 48c7c8d089ddabc0947a1945028904996961f908

--ui-splits: Fix off-by-one error on last split comparison time
We were improperly displaying the comparison time for the last split too early
due to taking cl-subseq of (1- (length splits) of the target-splits/current splits.
The END parameter isn't zero-based, so length is actually the bound we want.

Bug introduced in:
042f978550dd948ae135b01725c8cde6be8d1fc4

speedo.el
@@ -904,13 +904,13 @@ Reset timers."
                             (speedo--relative-time
                              (speedo--splits-duration
                               (cl-subseq target-splits 0
-                                         (min (1+ index) (1- (length target-splits)))))
+                                         (min (+ index 1) (length target-splits))))
                              (speedo--splits-duration
                               (when-let ((splits (plist-get (if speedo--review
                                                                 (speedo-target-last-attempt)
                                                               speedo--current-attempt)
                                                             :splits)))
-                                (cl-subseq splits 0 (min (1+ index) (1- (length splits))))))))
+                                (cl-subseq splits 0 (min (+ index 1) (length splits)))))))
                           speedo-text-place-holder)))
                 (when current-line (setq s (propertize s 'comparison-timer t)))
                 (if best-split (propertize s 'face 'speedo-pb) s)))