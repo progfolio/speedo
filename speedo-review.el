af51975c191dddda74011aa6460932df4924ed3a
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-10-28 13:29:41 -0400

    review: Correct review header grammar for single attempt

--- speedo-review.el
+++ speedo-review.el
@@ -400,7 +400,8 @@ HEADER is shown in the review buffer."
     (setq speedo-review--header
           (or header
               (list (speedo--header-game-info)
-                    (format " %d Attempts" (length attempts)))))
+                    (let ((len (length attempts)))
+                      (format " %d Attempt%s" len (if (> len 1) "s" ""))))))
     (speedo-review--ui-init attempts 'cache)
     (display-buffer speedo-review-buffer)))
 
@@ -424,7 +425,11 @@ HEADER is displayed in review buffer."
     (when (> n 0) (setq attempts (reverse attempts)))
     (speedo-review attempts
                    (list (speedo--header-game-info)
-                         (or header (format " Last %d Attempts" (length attempts)))))))
+                         (or header
+                             (let ((len (length attempts)))
+                               (if (eq len 1)
+                                   "Last Attempt"
+                                 (format " Last %d Attempts" len))))))))
 
 ;;;###autoload
 (defun speedo-review-last-runs (&optional n attempts header)
@@ -439,7 +444,10 @@ HEADER is displayed in review buffer."
     (when (> n 0) (setq attempts (reverse attempts)))
     (speedo-review attempts (list (speedo--header-game-info)
                                   (or header
-                                      (format " Last %d Runs" (length attempts)))))))
+                                      (let ((len (length attempts)))
+                                        (if (eq len 1)
+                                            " Last Run"
+                                          (format " Last %d Runs" len))))))))
 
 ;;;###autoload
 (defun speedo-review-top-runs (&optional n attempts)
@@ -453,7 +461,10 @@ If N is negative, they are sorted most recent last."
          (top (cl-subseq runs 0 (min (abs n) (length runs)))))
     (when (< n 0) (setq top (reverse top)))
     (speedo-review top (list (speedo--header-game-info)
-                             (format " Top %d Runs" (length top))))))
+                             (let ((len (length top)))
+                               (if (eq len 1)
+                                   " Personal Best"
+                                 (format " Top %d Runs" len)))))))
 
 (defmacro speedo-review-def-col-toggle (name)
   "Define a toggle command for column represented by NAME."
