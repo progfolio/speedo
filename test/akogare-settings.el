cb9fb1995edd4de1fb016c73ebaaadf9ce893a74
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2022-01-08 20:08:48 -0500

    Use face-remap-set-base instead of face-remap-add-relative
    Otherwise we get wild face height due to inheritance.

--- test/akogare-settings.el
+++ test/akogare-settings.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: t; -*-
-(face-remap-add-relative 'speedo-timer :height 3.0)
+(face-remap-set-base 'speedo-timer :height 3.0)
 (with-current-buffer (get-buffer-create speedo-buffer)
   (unless (string= (buffer-name) "*Akogare Mario World*")
     (setq speedo-buffer (rename-buffer "*Akogare Mario World*"))))
